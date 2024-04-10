21281d43974c7cc458fc268d381d32c27e967555
Noah Ickenroth <noahickenroth@uni-koblenz.de>
2024-04-10 18:51:14 +0200

    asdasf

--- connections.js
+++ connections.js
@@ -46,17 +46,8 @@ document.addEventListener('DOMContentLoaded', function() {
         if(isMobile) {
             const cont1 = document.getElementById('container1');
             const cont2 = document.getElementById('container2');
-            cont1.style.width = "60vw";
-            cont2.style.width = "60vw";
             cont1.style.margin = "0 auto";
             cont2.style.margin = "0 auto";
-            const enterButton = document.getElementById('enterButton');
-            enterButton.fontSize = "5vw";
-            const shuffleButton = document.getElementById('shuffleButton');
-            shuffleButton.fontSize = "5vw";
-            const shareButton = document.getElementById('shButton');
-            shareButton.fontSize = "5vw";
-
         }
         const newShareButton = document.getElementById('shButton');
         newShareButton.style.backgroundColor = "white";
