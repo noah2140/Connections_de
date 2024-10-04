864e09caebe7c0ddfbd6e3c207cb8228ba972dae
Noah Ickenroth <noahickenroth@uni-koblenz.de>
2024-10-04 10:57:13 +0200

    removed log messaging

--- connections.js
+++ connections.js
@@ -318,8 +318,6 @@ document.addEventListener('DOMContentLoaded', function() {
         // Start with a large font size and iteratively reduce until it fits
         let fontSize = 100; // Start with a large font size
         tempDiv.style.fontSize = `${fontSize}px`;
-
-        console.log(tempDiv.offsetWidth + " " + containerWidth);
     
         // Reduce font size until the text fits within the container
         while ((tempDiv.offsetWidth > containerWidth) && fontSize > 1) {
